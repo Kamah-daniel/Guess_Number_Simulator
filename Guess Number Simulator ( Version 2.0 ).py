dcec65ad4efb1e0e7e5868952aaeb1d6b927eebe
KAMAH DANIEL SOMTOCHKWU <s.kamahjnr@gmail.com>
2019-05-06 15:08:46 -0700

    Add files via upload

---
+++ Guess Number Simulator ( Version 2.0 ).py
@@ -0,0 +1,27 @@
+import random
+memorized_number = random.randint(1, 10)
+print("Hi!,Am Thinking of a number beetween 1 and 10, guess my number")
+
+
+def guess_number():
+    guess_count = 0
+    while (guess_count < 3):
+        try:
+            guessed_number = int(input('>>>').lower())
+            guess_count += 1
+            if (guessed_number == memorized_number):
+                print('Wow!, You guessed my number correctly')
+                break
+            elif (guessed_number < memorized_number):
+                print('Number is LOW!')
+            elif(guessed_number > memorized_number):
+                print('Number is HIGH!')
+        except (ValueError):
+            print('Please Enter a Valid Value')
+    else:
+        print("You couldn't guess my number in " + str(guess_count) + " trials")
+        print("I was thinking of " + str(memorized_number))
+    return guess_number
+
+
+guess_number() 
